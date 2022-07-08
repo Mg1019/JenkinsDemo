0e27b876e83a35efe5d3cc34f6019f0a597910ff
mgenovese <m.genovese1019@gmail.com>
2022-07-08 14:58:10 -0400

    Adding jenkins file.

--- JenkinsDemo/Program.cs
+++ JenkinsDemo/Program.cs
@@ -1,7 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-using JenkinsDemo;
-
-Console.WriteLine("Hello, World!");
+﻿using JenkinsDemo;
 
 var sumClass = new SimpleSum();
 var sum = sumClass.Add(2, 2);
+Console.WriteLine(sum);
